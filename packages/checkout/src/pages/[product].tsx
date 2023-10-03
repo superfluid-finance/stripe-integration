b47d75eb0ff395705438d87c83d58dab887fb7d9
Kaspar Kallas <kasparkallas@outlook.com>
2023-10-03 08:37:26 +0000

    wip

--- packages/checkout/src/pages/[product].tsx
+++ packages/checkout/src/pages/[product].tsx
@@ -4,24 +4,43 @@ import { ConnectKitProvider, ConnectKitButton, getDefaultConfig, useModal } from
 import { FC, useMemo, useState } from 'react';
 import WagmiProvider from '@/components/WagmiProvider';
 import SupefluidWidgetProvider from '@/components/SuperfluidWidgetProvider';
+import { useRouter } from 'next/router';
+import { useQuery } from 'wagmi';
+import Stripe from "stripe";
 
 const inter = Inter({ subsets: ['latin'] })
 
+
+
 export default function Home() {
   const [initialChainId, setInitialChainId] = useState<number | undefined>();
+
+  const { isReady, query } = useRouter();
+
+  const productId = useMemo<string | undefined>(() => {
+    if (!isReady)
+      return;
+
+    if (typeof query.product !== "string")
+      return;
+
+    return query.product
+  }, [isReady, query.product]);
+
+  // const { } = useQuery([productId], async () => {
+  //   stripe
+  // });
 
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <WagmiProvider>
-          <ConnectKitProvider options={{
-            initialChainId
-          }}>
-            <SupefluidWidgetProvider setInitialChainId={setInitialChainId} />
-          </ConnectKitProvider>
-        </WagmiProvider>
+        <ConnectKitProvider options={{
+          initialChainId
+        }}>
+          {/* <SupefluidWidgetProvider setInitialChainId={setInitialChainId} /> */}
+        </ConnectKitProvider>
       </div>
     </main>
   )
--- packages/checkout/src/pages/_app.tsx
+++ packages/checkout/src/pages/_app.tsx
@@ -1,6 +1,7 @@
+import WagmiProvider from '@/components/WagmiProvider'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <WagmiProvider><Component {...pageProps} /></WagmiProvider>
 }
