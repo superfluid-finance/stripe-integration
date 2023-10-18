5b191feb17e8d2619a5a996c788426314b3c6b67
Kaspar Kallas <kasparkallas@outlook.com>
2023-10-18 08:57:40 +0000

    add front-end to k8s set-up

--- apps/frontend/src/publicConfig.ts
+++ apps/frontend/src/publicConfig.ts
@@ -3,7 +3,7 @@ import ensureDefined from './utils/ensureDefined';
 const publicConfig = {
   walletConnectProjectId: ensureDefined(
     process.env.NEXT_PUBLIC_WALLECT_CONNECT_PROJECT_ID,
-    'NEXT_PUBLIC_WALLECT_CONNECT_PROJECT_ID',
+    'NEXT_PUBLIC_WALLECT_CONNECT_PROJECT_ID', // TODO(KK): When dockerizing then this needs to be changed away from being "NEXT_PUBLIC_" variable
   ),
 } as const;
 
