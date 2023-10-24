cdbb72e2c8c54b063b182476f7ff8047029eb737
Kaspar Kallas <kasparkallas@outlook.com>
2023-10-24 17:54:08 +0000

    remove ensureDefined, for now...

--- apps/frontend/src/publicConfig.ts
+++ apps/frontend/src/publicConfig.ts
@@ -5,10 +5,7 @@ import getConfig from 'next/config';
 const { publicRuntimeConfig } = getConfig();
 
 const publicConfig = {
-  walletConnectProjectId: ensureDefined(
-    publicRuntimeConfig.NEXT_PUBLIC_WALLECT_CONNECT_PROJECT_ID,
-    'NEXT_PUBLIC_WALLECT_CONNECT_PROJECT_ID', // TODO(KK): When dockerizing then this needs to be changed away from being "NEXT_PUBLIC_" variable
-  ),
+  walletConnectProjectId: publicRuntimeConfig.NEXT_PUBLIC_WALLECT_CONNECT_PROJECT_ID,
 } as const;
 
 export default Object.freeze(publicConfig);
