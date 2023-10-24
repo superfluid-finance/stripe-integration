f6c320a7c516156d871c3453560ac180587b887a
Kaspar Kallas <kasparkallas@outlook.com>
2023-10-24 21:05:54 +0000

    try to fix cjs runtime issue

--- apps/frontend/next.config.mjs
+++ apps/frontend/next.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const config = {
   output: 'standalone',
   reactStrictMode: true,
   publicRuntimeConfig: {
     NEXT_PUBLIC_WALLECT_CONNECT_PROJECT_ID: process.env.NEXT_PUBLIC_WALLECT_CONNECT_PROJECT_ID,
   },
+  transpilePackages: [
+    "@superfluid-finance/tokenlist"
+  ]
 };
+
+export default config;
