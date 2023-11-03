c40936ac04c256d34a0548776adcd21b76997f23
tokdaniel <tokdaniel4@gmail.com>
2023-11-03 17:58:05 +0100

    Feature: Cache with Redis (#20)
    * cache with providers
    
    * linting, and ordering fixes
    
    * use redis password and set ttl to much shorter time
    
    ---------
    
    Co-authored-by: Kaspar Kallas <kasparkallas@outlook.com>

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { BullModule } from '@nestjs/bullmq';
+import { redisStore } from 'cache-manager-redis-store';
+import { CacheInterceptor, CacheModule, CacheStore } from '@nestjs/cache-manager';
 import { QueueDashboardModule } from './queue-dashboard/queue-dashboard.module';
 import { CheckoutSessionModule } from './checkout-session/checkout-session.module';
 import { StripeListenerModule } from './stripe-listener/stripe-listener.module';
@@ -10,6 +12,7 @@ import { SuperTokenAccountingModule } from './super-token-accounting/super-token
 import { SuperfluidStripeConverterModule } from './superfluid-stripe-converter/superfluid-stripe-converter.module';
 import { HealthModule } from './health/health.module';
 import { registerStripeModule } from './stripe-module-config';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 const registerConfigModule = () =>
   ConfigModule.forRoot({
@@ -19,12 +22,12 @@ const registerConfigModule = () =>
 const registerBullModule = () =>
   BullModule.forRootAsync({
     inject: [ConfigService],
-    useFactory: async (configService: ConfigService) => ({
+    useFactory: async (config: ConfigService) => ({
       connection: {
-        host: configService.getOrThrow('REDIS_HOST'),
-        port: configService.getOrThrow('REDIS_PORT'),
-        password: configService.get('REDIS_PASSWORD'),
-        username: configService.get('REDIS_USER'),
+        host: config.getOrThrow('REDIS_HOST'),
+        port: config.getOrThrow('REDIS_PORT'),
+        password: config.get('REDIS_PASSWORD'),
+        username: config.get('REDIS_USER'),
       },
       defaultJobOptions: {
         attempts: 3,
@@ -36,9 +39,32 @@ const registerBullModule = () =>
     }),
   });
 
+const registerCacheModule = () =>
+  CacheModule.registerAsync({
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory: async (config: ConfigService) => {
+      const store = await redisStore({
+        socket: {
+          host: config.getOrThrow('REDIS_HOST'),
+          port: Number(config.getOrThrow('REDIS_PORT')),
+        },
+        username: config.get('REDIS_USER'),
+        password: config.get('REDIS_PASSWORD')
+      });
+
+      return {
+        isGlobal: true,
+        store: store as unknown as CacheStore, // Nest.js hasn't caught up with right types
+        ttl: 3000, // In cache-manager v5, TTL is configured in milliseconds: https://docs.nestjs.com/techniques/caching
+      };
+    },
+  });
+
 @Module({
   imports: [
     registerConfigModule(),
+    registerCacheModule(),
     registerStripeModule(),
     registerBullModule(),
     QueueDashboardModule,
@@ -50,6 +76,11 @@ const registerBullModule = () =>
     HealthModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
+    },
+  ],
 })
 export class AppModule {}
